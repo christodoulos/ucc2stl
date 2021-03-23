e50f701012bff711a718c7715531fdbad64a5cb5
Christodoulos Fragkoudakis <chfrag@mail.ntua.gr>
2021-03-23 12:06:12 +0200

    ci and test-build srcripts, deleted dictionary.ts

---
+++ test-build.js
@@ -0,0 +1,8 @@
+const assert = require("assert");
+const ucc2stl = require("./dist");
+const c = new ucc2stl.Point([Math.random(), Math.random(), Math.random()]);
+const d = new ucc2stl.Point([Math.random(), Math.random(), Math.random()]);
+const e = c.subtract(d);
+const f = d.subtract(c);
+assert.deepStrictEqual(c.add(e).equals(d), true);
+assert.deepStrictEqual(d.add(f).equals(c), true);
